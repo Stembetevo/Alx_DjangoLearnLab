61bf372236f25619b7ca7d6f0b53913d7df350b8
Stembetevo <stephenkaruru05@gmail.com>
2025-11-17 20:50:55 +0300

    Fix UserCreationForm instantiation in SignUpView for proper form handling

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -45,7 +45,7 @@ class LibraryDetailView(DetailView):
 
 #Registration View
 class SignUpView(CreateView):
-    form_class = UserCreationForm
+    form_class = UserCreationForm()
     success_url = reverse_lazy('login')
     # templates are placed under relationship_app/templates/relationship_app/
     template_name = 'relationship_app/registration.html'
