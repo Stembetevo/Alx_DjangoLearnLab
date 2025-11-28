5ab268ba98cd28508689011d50f14666a27ef7f2
Stembetevo <stephenkaruru05@gmail.com>
2025-11-29 02:19:06 +0300

    Add BookViewSet for CRUD operations and update URL routing

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
+
 # Create your views here.
 
 class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    """ViewSet providing CRUD operations for the Book model."""
     queryset = Book.objects.all()
     serializer_class = BookSerializer
