be0ace152184b366eb5a6b420c13299fdd8f5da7
Stembetevo <stephenkaruru05@gmail.com>
2025-12-03 00:24:55 +0300

    Remove redundant import statements in views.py for cleaner code

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,7 +1,8 @@
-from rest_framework import viewsets, generics, permissions
+from rest_framework import viewsets, generics
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from .models import Author, Book
 from .serializers import BookSerializer, AuthorSerializer
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+
 
 class AuthorViewSet(viewsets.ModelViewSet):
 	queryset = Author.objects.all()
