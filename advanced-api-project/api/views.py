c633ae27b91fb9219feaed3705dbdc55128f7d27
Stembetevo <stephenkaruru05@gmail.com>
2025-12-03 01:23:55 +0300

    Add DjangoFilterBackend to book list view for enhanced filtering capabilities

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,5 +1,8 @@
+from django_filters import rest_framework
 from rest_framework import viewsets, generics
+from rest_framework import generics
 from rest_framework.filters import SearchFilter, OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from .models import Author, Book
 from .serializers import BookSerializer, AuthorSerializer
@@ -18,7 +21,7 @@ class ListView(generics.ListAPIView):
 	permission_classes = [IsAuthenticatedOrReadOnly]
 
 	# Enable django-filter backend, search and ordering
-	filter_backends = [SearchFilter, OrderingFilter]
+	filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
 	# Allow filtering by these fields (including related lookups)
 	filterset_fields = ['title', 'publication_year', 'author', 'author__name']
 	# Enable text search on title and author name
