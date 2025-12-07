ffdb0be0d1962ef04a84dc158452f722639baa9b
Stembetevo <stephenkaruru05@gmail.com>
2025-12-07 15:29:31 +0300

    Fix blog post URLs to use singular 'post' instead of plural 'posts'

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -13,11 +13,11 @@ urlpatterns = [
     path('profile/edit/', views.edit_profile, name='edit_profile'),
     
     # Blog Post CRUD URLs
-    path('posts/', views.PostListView.as_view(), name='post-list'),
-    path('posts/new/', views.PostCreateView.as_view(), name='post-create'),
-    path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
-    path('posts/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post-update'),
-    path('posts/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
+    path('post/', views.PostListView.as_view(), name='post-list'),
+    path('post/new/', views.PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
+    path('post/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
     
     # Home URL
     path('', views.home, name='home'),
